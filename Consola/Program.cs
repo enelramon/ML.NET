28185857f60d927fad50d87cfb051a162c348ba6
Enel Almonte <enelramon@hotmail.com>
2019-12-01 21:26:46 -0400

    Cambiando estructura

---
+++ Consola/Program.cs
@@ -0,0 +1,21 @@
+﻿using MyMLAppML.Model;
+using System;
+
+namespace myMLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var input = new ModelInput();
+            input.SentimentText = Console.ReadLine();
+
+            var output = ConsumeModel.Predict(input);
+
+            Console.WriteLine($"Es negativo? {output.Prediction}");
+
+        }
+    }
+}
